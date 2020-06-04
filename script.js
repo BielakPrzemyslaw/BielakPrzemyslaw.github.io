d269f44eb9833fb898fb5f0398ebfde3a6ae1f22
BielakPrzemyslaw <przemyslawbielak@interia.pl>
2020-06-04 08:26:21 +0200

    Task

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://glacial-meadow-66126.herokuapp.com/v1/task/';
+  const apiRoot = 'https://glacial-meadow-66126.herokuapp.com/v1/task';
   const trelloApiRoot = 'https://glacial-meadow-66126.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
